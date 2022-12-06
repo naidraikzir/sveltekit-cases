a825f1b91c349cda9aaae2e69fd79504c5464809
Rizki Ardian <radeite8@gmail.com>
2022-12-06 21:34:57 +0700

    Add error page

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <div class="flex items-center justify-between p-4 font-serif">
-	<a href="/" class="font-bold text-2xl">SVLTKIT</a>
+	<a href="/" class="w-[86px] font-bold text-2xl">SVLTKIT</a>
 	<Menu />
-	<div class="pl-14">
+	<div class="w-[86px] text-right">
 		<Themer />
 	</div>
 </div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import Card from '$lib/components/Card.svelte'
+	import Meta from '$lib/components/Meta.svelte'
+</script>
+
+<Meta title={`${$page.status}`} />
+
+<div class="container mx-auto px-4 min-h-[20rem] flex items-center justify-center">
+	<Card>
+		<div class="text-center">
+			<h1 class="text-5xl">{$page.status}</h1>
+			<h2 class="text-2xl">{$page.error?.message}</h2>
+		</div>
+	</Card>
+</div>
