9ab52327d1f5f5bdfb57e3dfba5688682e9edea2
Rizki Ardian <radeite8@gmail.com>
2022-12-19 17:43:38 +0700

    Lighthouse score

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,7 +3,7 @@
 	import Menu from './Menu.svelte'
 	import Themer from './Themer.svelte'
 
-	const sideWidthClass = 'w-[64px]'
+	const sideWidthClass = 'w-[4.5rem]'
 </script>
 
 <div class="flex items-center justify-between p-4">
@@ -12,8 +12,9 @@
 	<div class="flex {sideWidthClass} text-right">
 		<a
 			href="https://github.com/naidraikzir/sveltekit-cases"
-			class="w-8 h-8 flex items-center justify-center"
+			class="w-8 h-8 mr-2 flex items-center justify-center"
 			target="_blank"
+			aria-label="Github"
 			rel="noreferrer"
 		>
 			<GithubLogo />
