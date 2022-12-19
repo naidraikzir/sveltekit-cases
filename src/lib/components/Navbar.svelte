d83167ba7b24b80647521bd51c43f44918c1b77b
Rizki Ardian <radeite8@gmail.com>
2022-12-19 11:52:53 +0700

    Add repo link

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
+	import GithubLogo from './GithubLogo.svelte'
 	import Menu from './Menu.svelte'
 	import Themer from './Themer.svelte'
+
+	const sideWidthClass = 'w-[64px]'
 </script>
 
 <div class="flex items-center justify-between p-4">
-	<a href="/" class="w-[42px] font-bold text-2xl">Kit.</a>
+	<a href="/" class="{sideWidthClass} font-bold text-2xl">Kit.</a>
 	<Menu />
-	<div class="w-[42px] text-right">
+	<div class="flex {sideWidthClass} text-right">
+		<a
+			href="https://github.com/naidraikzir/sveltekit-cases"
+			class="w-8 h-8 flex items-center justify-center"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<GithubLogo />
+		</a>
 		<Themer />
 	</div>
 </div>
